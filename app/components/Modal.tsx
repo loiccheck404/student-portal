e14f2766bd483e7fdc08e913c8fd990814958d6e
loiccheck404 <ndzieloic334@gmail.com>
2025-11-28 11:01:21 +0100

    8th commit

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -153,7 +153,7 @@ export default function Modal({
     <div className="fixed inset-0 z-50 overflow-y-auto">
       {/* Backdrop */}
       <div
-        className="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
+        className="fixed inset-0 backdrop-blur-sm transition-opacity"
         onClick={onClose}
       />
 
